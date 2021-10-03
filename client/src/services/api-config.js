bff97df36c1c95fdc19f6af3e0ae3bda2a11472f
Robert Balonek <robertbalonek@gmail.com>
2021-10-02 22:24:32 -0700

    Netlify

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:3000"
-    : // ? "https://health-fair-signup.herokuapp.com"
-      "http://localhost:3000";
+    ? "https://signup-template.herokuapp.com/" //ADD
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
